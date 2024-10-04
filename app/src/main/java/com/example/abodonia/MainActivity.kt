465f515df3e3223614bb1ab363513248791dcd30
Gamal <htbtjmal2013@gmail.com>
2024-10-04 19:07:26 +0300

    the first Commit

---
+++ app/src/main/java/com/example/abodonia/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.abodonia
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+}
+
+}
